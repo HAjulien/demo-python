ac9598ec80a27527ef8d85b9b9c60fd7121fafae
HAjulien <hajulien1@gmail.com>
2022-01-10 13:06:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-a = 50
-print (a + 3)
+a = 7
+
+# print(a + 3)
+
+b = 3
+
+# print( a + b)
+
+# a = b + 1
+
+# print (a)
+
+#a += b  # equivalent a = a + b  a*= b a/=b 
+#print (a)
+
+c = a//b
+d = a%b
+print('La division de ' +str(a)+ ' par ' +str(b)+ ' est égale à ' +str(c)+'.Il reste ' +str(d)+ '.' )
+print(d)
+
+chaine= 'Et voilà du texte'
+chaine= 'nous l\'avons'
+chaine2= "\"réparé\""
+print(chaine +" "+ chaine2)
