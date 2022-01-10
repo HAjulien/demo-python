4d361009094093efe45c943c9418d8d794cab7ca
HAjulien <hajulien1@gmail.com>
2022-01-10 15:03:32 +0100

    update

--- test.py
+++ test.py
@@ -21,4 +21,32 @@ print(d)
 chaine= 'Et voilà du texte'
 chaine= 'nous l\'avons'
 chaine2= "\"réparé\""
-print(chaine +" "+ chaine2)
+# print(chaine +" "+ chaine2)
+
+#reponse= input()    #12une ligne vide apparaît et attend que l'utilisateur entre une info
+
+age = int(input("quel est votre âge? "))  #transforme string en integrer pour par exemple fonction if age > 16
+
+print("Vous avez " +str(age)+ " ans.")
+
+if age > 16 and age < 100:
+    print("Vous avez plus de 16 ans.")
+# elif age < 0:      #elif est un if else
+#     print("tu te moquerais pas de moi? ")
+elif age > 100 or age < 0:      #elif est un if else
+    print("tu te moquerais pas de moi? ")
+else :
+    print("Tu es encore trop jeune")
+
+a = 3
+print(type(a))
+
+b = 8.2
+print(type(b))
+
+chaine = "Au revoir"
+print(type(chaine))
+vrai= True
+print(type(vrai))
+
+print(7 > 11 < 9 !=10)  # != différent
---
+++ test2.py
@@ -0,0 +1,14 @@
+# i = 0
+# while i < 10:
+#     i += 1
+#     if i%2 == 0:   # si i est paire
+#         continue
+#     print(i)
+
+a = int(input("a:"))
+b = int(input("b:"))
+
+a, b = b, a
+
+print(a)
+
