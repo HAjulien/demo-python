b5777f402e5f09d80ff374b78d75cfb30e12a6a9
HAjulien <hajulien1@gmail.com>
2022-01-10 17:01:16 +0100

    Update test4.py

--- test4.py
+++ test4.py
@@ -15,3 +15,8 @@ print(a)
 change(10)
 
 affichage()
+nombre = 7
+nombre = int(input("la table de multuplication que vous voulez voir: "))
+print("Voici la table de multiplication de ", nombre)
+for x in range(0,11):   #exclu le 11 mais inclu le 0
+    print(x, "x ", nombre, "= ", x*nombre )
