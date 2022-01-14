5a66eba69a698bf6e2efc41744394ed5b3e49267
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-01-14 10:57:36 +0100

    Update exercice2.py

--- exercice2.py
+++ exercice2.py
@@ -10,6 +10,8 @@ resultat= a*a
 while nMin > resultat or resultat > nMax:
     if resultat > nMax:
         a = (a/2)
+        resultat= a*a
     elif resultat < nMin:
         a= ((a + (a/2))/2)
+        resultat= a*a
 print("le resultat est " + str(resultat))
